15ffafde2b07dc0b3a40016f73da488c1600c5b8
James Patrick Keegan <atomicbomber.email@gmail.com>
2021-02-05 11:21:06 +0700

    Updates.

--- test.py
+++ test.py
@@ -9,31 +9,36 @@ import matplotlib.pyplot as plt
 
 sns.set(
     font_scale=0.65,
-    rc={'figure.figsize':(22,20)},
+    rc={'figure.figsize': (22, 20)},
     font="Monospace",
 )
 
-def report_confusion_matrix(
+
+def generate_and_save_report(
     y_pred: list,
     y_true: list,
     corpus_name: str,
     algorithm_name: str,
     fold: int,
 ) -> None:
-    confusion_matrix = ConfusionMatrix(
-        actual_vector=y_true,
-        predict_vector=y_pred,
-    )
-
-    confusion_matrix_positions = DataFrame(confusion_matrix.position())
-    confusion_matrix_positions = confusion_matrix_positions.applymap(lambda positions: len(positions))
-
     report = DataFrame(classification_report(
         y_pred,
         y_true,
         zero_division=0,
         output_dict=True,
     ))
+
+    report.to_excel(
+        "./reports/{}-{}-fold-{}.xlsx".format(corpus_name, algorithm_name, fold))
+
+    confusion_matrix = ConfusionMatrix(
+        actual_vector=y_true,
+        predict_vector=y_pred,
+    )
+
+    confusion_matrix_positions = DataFrame(confusion_matrix.position())
+    confusion_matrix_positions = confusion_matrix_positions.applymap(
+        lambda positions: len(positions))
 
     report_text = ""
     for pos_class in confusion_matrix.classes:
@@ -43,24 +48,9 @@ def report_confusion_matrix(
         fn = confusion_matrix_positions[pos_class]["FN"]
         accuracy = tp + tn / (tp + tn + fp + fn)
 
-        report_text += """
-Untuk pengujian pada fold ke-{fold_counter} dari algoritma {algorithm_name}, kelas '{pos_class}' memiliki nilai true positive (tp) = {tp:{int_format}}, true negative (tn) = {tn:{int_format}}, false positive (fp) = {fp:{int_format}}, false negative (fn) = {fn:{int_format}}. Nilai recall = tp / tp + fn = {recall:{dec_format}}, nilai precision = tp / tp + fp = {precision:{dec_format}}, nilai f1-score = 2tp / (2tp + fp + fn) = {f1_score:{dec_format}}, accuracy = tp + tn / tp + tn + fp + fn = {accuracy:{dec_format}}.
-""".format(
-    algorithm_name=algorithm_name,
-    pos_class=pos_class,
-    tn=tn,
-    fn=fn,
-    tp=tp,
-    fp=fp,
-    fold_counter=fold_counter,
-    precision=report[pos_class]["precision"],
-    recall=report[pos_class]["recall"],
-    f1_score=report[pos_class]["f1-score"],
-    accuracy=accuracy,
-    int_format="d",
-    dec_format=".4f",
-).strip()
-    
+        report_text += get_pos_class_report_text(
+            algorithm_name, report, pos_class, tp, tn, fp, fn, accuracy)
+
     with open("./reports/{}-{}-fold-{}.txt".format(corpus_name, algorithm_name, fold_counter), "w") as report_filehandle:
         report_filehandle.write(report_text)
 
@@ -68,15 +58,16 @@ Untuk pengujian pada fold ke-{fold_counter} dari algoritma {algorithm_name}, kel
     confusion_matrix_df = DataFrame(confusion_matrix.to_array())
     row_sums = confusion_matrix_df.sum(axis=1).to_list()
 
-    x_labels = ["{} ({})".format(label, row_sums[index]) for index, label in enumerate(pos_classes)]    
+    x_labels = ["{} ({})".format(label, row_sums[index])
+                for index, label in enumerate(pos_classes)]
 
     temp_df = confusion_matrix_df.T
 
     shape = temp_df.shape
     annotations = [
-        [f'''{pos_classes[col]}\n{pos_classes[row]}\n{temp_df[row][col]}''' 
+        [f'''{pos_classes[col]}\n{pos_classes[row]}\n{temp_df[row][col]}'''
             for col in range(0, shape[0])]
-                for row in range(0, shape[1])
+        for row in range(0, shape[1])
     ]
 
     confusion_matrix_heatmap = sns.heatmap(
@@ -93,10 +84,17 @@ Untuk pengujian pada fold ke-{fold_counter} dari algoritma {algorithm_name}, kel
 
     fig = confusion_matrix_heatmap.get_figure()
     fig.savefig(
-        "./reports/{}-{}-fold-{}.svg".format(corpus_name, algorithm_name, fold,),
+        "./reports/{}-{}-fold-{}.svg".format(corpus_name,
+                                             algorithm_name, fold,),
         bbox_inches='tight'
     )
     plt.clf()
+    pass
+
+
+def get_pos_class_report_text(algorithm_name, report, pos_class, tp, tn, fp, fn, accuracy):
+    return """ Untuk pengujian pada fold ke-{fold_counter} dari algoritma {algorithm_name}, kelas '{pos_class}' memiliki nilai true positive (tp) = {tp:{int_format}}, true negative (tn) = {tn:{int_format}}, false positive (fp) = {fp:{int_format}}, false negative (fn) = {fn:{int_format}}. Nilai recall = tp / tp + fn = {recall:{dec_format}}, nilai precision = tp / tp + fp = {precision:{dec_format}}, nilai f1-score = 2tp / (2tp + fp + fn) = {f1_score:{dec_format}}, accuracy = tp + tn / tp + tn + fp + fn = {accuracy:{dec_format}}. """.format(algorithm_name=algorithm_name, pos_class=pos_class, tn=tn, fn=fn, tp=tp, fp=fp, fold_counter=fold_counter, precision=report[pos_class]["precision"], recall=report[pos_class]["recall"], f1_score=report[pos_class]["f1-score"], accuracy=accuracy, int_format="d", dec_format=".4f", ).strip()
+
 
 for corpus_name in corpora:
     for fold_counter in range(1, N_FOLD + 1):
@@ -108,7 +106,8 @@ for corpus_name in corpora:
         x = [item for sublist in x_raw for item in sublist]
 
         for model_type in model_types:
-            print("Memroses corpus {}, fold {}, metode {}.".format(corpus_name, fold_counter, model_type))
+            print("Memroses corpus {}, fold {}, metode {}.".format(
+                corpus_name, fold_counter, model_type))
 
             model = pickle.load(
                 open(get_model_path(corpus_name, model_type, fold_counter), "rb")
@@ -121,14 +120,7 @@ for corpus_name in corpora:
             y_true_raw = [[part[1] for part in line] for line in test_data]
             y_true = [item for sublist in y_true_raw for item in sublist]
 
-            report = pd.DataFrame(classification_report(
-                y_pred,
-                y_true,
-                zero_division=0,
-                output_dict=True,
-            ))
-
-            report_confusion_matrix(y_pred, y_true, corpus_name, model_type, fold_counter)
-            report.to_excel("./reports/{}-{}-fold-{}.xlsx".format(corpus_name, model_type, fold_counter))
+            generate_and_save_report(
+                y_pred, y_true, corpus_name, model_type, fold_counter)
         pass
     pass
